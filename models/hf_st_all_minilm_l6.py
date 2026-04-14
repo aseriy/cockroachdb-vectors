55357a205142815dd94dfc595b7bf97213bfec18
Alex Seriy <alex.m.seriy@gmail.com>
2026-04-14 17:58:30 +0000

    Transparent local vs Nuclio execution

--- models/hf_st_all_minilm_l6.py
+++ models/hf_st_all_minilm_l6.py
@@ -8,6 +8,8 @@ from typing import Iterable, List, Tuple, Any
 from pathlib import Path
 import yaml
 import requests
+from urllib.parse import urljoin
+import inspect
 
 exec_local = True
 
@@ -23,12 +25,10 @@ if not os.getenv("NUCLIO"):
         for item in config['models'] 
         if isinstance(item, dict) and 'hf_st_all_minilm_l6' in item
     )
-    print(json.dumps(model_settings, indent=2))
 
     if 'nuclio' in model_settings:
         exec_local = False
 
-    print(f"exec_local: {exec_local}")
 # enf if
 #
 
@@ -70,7 +70,7 @@ def embedding_label() -> str:
         return "Hugging Face Sentence Transformer all-MiniLM-L6-v2"
 
     if 'nuclio' in model_settings:
-        url = model_settings['nuclio']['url']
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
         response = requests.get(url)
         response.raise_for_status()  # raises on non-200
         return response.text
@@ -78,34 +78,73 @@ def embedding_label() -> str:
 
 
 def embedding_description() -> str:
-    return textwrap.dedent(
-        """
-        General-purpose English sentence embedding model
-        based on MiniLM. Optimized for semantic similarity,
-        clustering, and retrieval tasks. Produces 384-dimensional
-        float vectors. Not multilingual.
-        https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
-        """
-    ).strip()
+    if exec_local:
+        return textwrap.dedent(
+            """
+            General-purpose English sentence embedding model
+            based on MiniLM. Optimized for semantic similarity,
+            clustering, and retrieval tasks. Produces 384-dimensional
+            float vectors. Not multilingual.
+            https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+            """
+        ).strip()
+
+    if 'nuclio' in model_settings:
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
+        response = requests.get(url)
+        response.raise_for_status()  # raises on non-200
+        return response.text
+
 
 
 def embedding_dim() -> int:
-    model = _MODEL_CACHE[huggingface_path]
-    return model.get_sentence_embedding_dimension()
+    if exec_local:
+        model = _MODEL_CACHE[huggingface_path]
+        return model.get_sentence_embedding_dimension()
+
+    if 'nuclio' in model_settings:
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
+        response = requests.get(url)
+        response.raise_for_status()  # raises on non-200
+        return response.text
+
 
 
 def embedding_index_opclass() -> str:
-    return "vector_cosine_ops"
+    if exec_local:
+        return "vector_cosine_ops"
+
+    if 'nuclio' in model_settings:
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
+        response = requests.get(url)
+        response.raise_for_status()  # raises on non-200
+        return response.text
 
 
 def embedding_index_operator() -> str:
-    return "<=>"
+    if exec_local:
+        return "<=>"
+
+    if 'nuclio' in model_settings:
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
+        response = requests.get(url)
+        response.raise_for_status()  # raises on non-200
+        return response.text
+
 
 
 def embedding_encode(input_text: str, verbose: bool = False) -> List[float]:
-    model = _MODEL_CACHE.get(huggingface_path)
-    embeddings = model.encode([input_text], batch_size=128, show_progress_bar=False)
-    return embeddings[0].tolist()
+    if exec_local:
+        model = _MODEL_CACHE.get(huggingface_path)
+        embeddings = model.encode([input_text], batch_size=128, show_progress_bar=False)
+        return embeddings[0].tolist()
+
+    if 'nuclio' in model_settings:
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
+        payload = {"text": input_text}
+        response = requests.post(url, json=payload)
+        response.raise_for_status()  # raises on non-200
+        return response.json()
 
 
 
@@ -117,18 +156,29 @@ def embedding_encode_batch(
   
     texts = [row_text for _, row_text in batch]
     row_ids = [row_id for row_id, _ in batch]
-    model = _MODEL_CACHE.get(huggingface_path)
 
-    if verbose:
-        for i, (row_id, row_text) in enumerate(zip(row_ids, texts), 1):
-            input_column_text = row_text[:40].replace('\n', '').replace('\r', '')
-            print(f"[INFO] (batch {batch_index}, {i}/{len(batch)}) Updating vector for row id {row_id}: '{input_column_text}'")
+    if exec_local:
+        model = _MODEL_CACHE.get(huggingface_path)
 
-    embeddings = model.encode(texts, batch_size=128, show_progress_bar=False)
+        if verbose:
+            for i, (row_id, row_text) in enumerate(zip(row_ids, texts), 1):
+                input_column_text = row_text[:40].replace('\n', '').replace('\r', '')
+                print(f"[INFO] (batch {batch_index}, {i}/{len(batch)}) Updating vector for row id {row_id}: '{input_column_text}'")
 
-    values = [(row_id, embedding.tolist()) for row_id, embedding in zip(row_ids, embeddings)]
+        embeddings = model.encode(texts, batch_size=128, show_progress_bar=False)
+        values = [[row_id, embedding.tolist()] for row_id, embedding in zip(row_ids, embeddings)]
+        return values
 
-    return values
+    if 'nuclio' in model_settings:
+        url = urljoin(model_settings['nuclio']['url'], inspect.currentframe().f_code.co_name)
+        payload = {
+            "index": batch_index,
+            "batch": [[row_id, row_text] for row_id, row_text in zip(row_ids, texts)]
+        }
+        response = requests.post(url, json=payload)
+        response.raise_for_status()  # raises on non-200
+        return response.json()
+
 
 
 
