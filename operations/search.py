eb0be31afa152b0ecc38e00fd3bc264634d8cfab
Alex Seriy <alex.m.seriy@gmail.com>
2026-04-24 19:39:34 +0000

    Switch to Jinja2 template for the search query

--- operations/search.py
+++ operations/search.py
@@ -1,11 +1,28 @@
 import atexit
 import importlib
+import textwrap
+from jinja2 import Template
 from psycopg2.pool import SimpleConnectionPool
 from .model import is_valid_model
 from .common import build_conn_kwargs, main_get_conn, get_primary_key_column
 
 
 model = None
+
+
+search_tmpl = \
+    """
+        SELECT
+            {{ primary_key }},
+            {{ source }},
+            ROUND({{ embedding }} {{ idxop }} %s::VECTOR({{ vector_dim }}), 6) AS distance
+        FROM {{ table }}
+        AS OF SYSTEM TIME follower_read_timestamp()
+        WHERE {{ embedding }} IS NOT NULL
+        ORDER BY {{ embedding }} {{ idxop }} %s::VECTOR({{ vector_dim }})
+        LIMIT %s
+    """
+search_tmpl = textwrap.indent(textwrap.dedent(search_tmpl).strip(), '\t')
 
 
 def run_search(args: dict):
@@ -22,29 +39,29 @@ def run_search(args: dict):
 
     primary_key, primary_key_type = get_primary_key_column(conn_pool, args['table'])
     if verbose:
-        print(f"[INFO] PK: {primary_key} ({primary_key_type})")
-        print()
+        print(f"[INFO] PK: {primary_key} ({primary_key_type})\n")
 
     vector = model.embedding_encode(args['text'], args['verbose'])
     vector_dim = model.embedding_dim()
     vector_param = "[" + ",".join(str(x) for x in vector) + "]"
     idxop = model.embedding_index_operator()
 
-    query = f"""
-            SELECT
-                {primary_key},
-                {args['source']},
-                ROUND({args['embedding']} {idxop} %s::VECTOR({vector_dim}), 6) AS distance
-            FROM {args['table']}
-            AS OF SYSTEM TIME follower_read_timestamp()
-            WHERE {args['embedding']} IS NOT NULL
-            ORDER BY {args['embedding']} {idxop} %s::VECTOR({vector_dim})
-            LIMIT {args['limit']}
-    """
-    
+    template = Template(search_tmpl)
+    query = textwrap.dedent(
+        template.render(
+            table = args['table'],
+            primary_key = primary_key,
+            source = args['source'],
+            embedding = args['embedding'],
+            vector_dim = vector_dim,
+            idxop = idxop
+        )
+    )
+    print(f"{query}\n")
+
     conn = main_get_conn(conn_pool)
     with conn.cursor() as cur:
-        cur.execute(query, (vector_param,vector_param))
+        cur.execute(query, (vector_param,vector_param, args['limit']))
         result = cur.fetchall()
 
     for r in result:
